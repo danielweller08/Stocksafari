c8b268b7dc680d00ff07d91c1ea49245399dde44
welleed0 <s09dwell@uni-bonn.de>
2023-06-29 14:32:19 +0200

    Create unit test for buystock

--- tests/testController.cpp
+++ tests/testController.cpp
@@ -2,3 +2,10 @@
 #include <stocksafari/Controller.hpp>
 
 using namespace StockSafari;
+
+TEST(ControllerTest, Buy_Stock) {
+    // Controller c;
+    // c.createaccount();
+    // c.deposit(100);
+    // c.set_stockValue("APPL", 10);
+}
