e1081ea7befc759b82718d68001095a367e5723a
paltzows0 <s35spalt@uni-bonn.de>
2023-06-29 16:12:36 +0200

    feat(auth): Implemented token auth
    - Added token decode to all relevant methods
    - Renamed create_account to registerAccount
    - Added login method
    - WARNING: Unit tests cause compile errors

--- examples/demo.cpp
+++ examples/demo.cpp
@@ -19,13 +19,13 @@ int main() {
 
     c.set_stockValue("APPL", 1);
 
-    Account& a = c.create_account("Damyan", "1234");
+    string token = c.registerAccount("Damyan", "1234");
 
-    a = c.deposit("Damyan", 500);
-    a = c.withdraw("Damyan", 1);
+    c.deposit("Damyan", 500, token);
+    c.withdraw("Damyan", 1, token);
 
     // Token test.
     auto token = c.generate_token("Test_Username");
     std::cout << "Token: " << token << std::endl;
-    std::cout << "Username after verification: " << c.decode_token(token) << std::endl;
+    std::cout << "Username after verification: " << c.try_decode_token(token) << std::endl;
 }
