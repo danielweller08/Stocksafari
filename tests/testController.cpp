baddd9b2870cb5d90c2f8016301dc187c70b8217
paltzows0 <s35spalt@uni-bonn.de>
2023-06-29 12:21:10 +0200

    feat(auth): Implemented authentication
    - Um das Programm ausführen zu können: 'sudo apt-get install libssl-dev'

--- tests/testController.cpp
+++ tests/testController.cpp
@@ -2,3 +2,10 @@
 #include <stocksafari/Controller.hpp>
 
 using namespace StockSafari;
+
+TEST(ControllerTest, Authentication_Works) {
+    Controller c;
+    auto token = c.generate_token("TEST");
+    auto result = c.decode_token(token);
+    ASSERT_EQ("TEST", result);
+}
