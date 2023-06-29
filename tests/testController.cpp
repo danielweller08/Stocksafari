2edeed0f20f5e81e86fc15a94b48190f2b4cf978
welleed0 <s09dwell@uni-bonn.de>
2023-06-29 16:27:06 +0200

    Create unit tests for buy and sell stocks

--- tests/testController.cpp
+++ tests/testController.cpp
@@ -1,16 +1,37 @@
 #include <gtest/gtest.h>
 #include <stocksafari/Controller.hpp>
+#include <iostream>
 
 using namespace StockSafari;
 
-// Test that quantity is too high
 TEST(ControllerTest, Buy_Stock) {
-    // Account person1 = Account("Daniel", "1234566");
+    Controller c;
+    Account& person1 = c.create_account("Daniel", "123456");
 
-    // Controller c;
-    // c.createaccount();
-    // c.deposit(100);
-    // c.set_stockValue("APPL", 10);
+    c.set_stockValue("APPL", 10);
+
+    c.deposit(person1.get_username(), 10);
+    EXPECT_THROW(c.buy_stock("APPL", 2, person1.get_username()) , invalid_argument);
+
+    c.deposit(person1.get_username(), 10);
+    Account& acc = c.buy_stock("APPL", 2, person1.get_username());
+    EXPECT_EQ(acc.get_balance(), 0);
+}
+
+TEST(ControllerTest, Sell_Stock) {
+    Controller c;
+    Account& person1 = c.create_account("Daniel", "123456");
+
+    c.set_stockValue("APPL", 10);
+
+    c.deposit(person1.get_username(), 10);
+
+    EXPECT_THROW(c.sell_stock("APPL", 1, person1.get_username()) , invalid_argument);
+
+    Account& acc = c.buy_stock("APPL", 1, person1.get_username());
+    EXPECT_EQ(acc.get_balance(), 0);
+    acc = c.sell_stock("APPL", 1, person1.get_username());
+    EXPECT_EQ(acc.get_balance(), 10);
 }
 
 TEST(ControllerTest, CreateAccountTest) {
