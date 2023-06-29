c24bb567a06c9746be0245216e85e0736c3fa5df
welleed0 <s09dwell@uni-bonn.de>
2023-06-29 14:47:16 +0200

    unit fcts

--- tests/testController.cpp
+++ tests/testController.cpp
@@ -3,9 +3,17 @@
 
 using namespace StockSafari;
 
+// Test that quantity is too high
 TEST(ControllerTest, Buy_Stock) {
+    // Account person1 = Account("Daniel", "1234566");
+
     // Controller c;
     // c.createaccount();
     // c.deposit(100);
     // c.set_stockValue("APPL", 10);
+}
+
+// Test that quantity is too high
+TEST(ControllerTest, Buy_Stock) {
+
 }
